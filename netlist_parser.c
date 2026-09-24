c2cc7e6bc989e3baad3d0fc7cf189a63f8dbac34
kyriakos15 <kypapoglou@gmail.com>
2026-09-24 16:58:27 +0300

    netlist parser update
    Many changes regarding how to store nodes

--- netlist_parser.c
+++ netlist_parser.c
@@ -4,59 +4,78 @@
 #include <unistd.h>
 
 
-#define MAX 3000 
-#define MAX_FAN_IN 4
- 
+#define MAX_NODES 300   // Minimum starting ammount of nodes that can be stored in the struct. If the .bench file has more nodes, the program will dynamically reallocate memory for the nodes.
+#define MAX_LINE 100     // Maximum ammount of characters in a line of the .bench file
+#define MAX_FAN_IN 5    // Maximum ammount of input edges for a node.
+#define MAX_WORD_LENGTH 15 // Maximum ammount of characters in a signals name
 
 
-typedef enum { 
+typedef enum {
     AND, OR, NOT, NAND, NOR, XOR, XNOR, BUF, INPUT, OUTPUT, DFF
-} FUNCTION;                                                                  // Many possible logic gates 
-
+} FUNCTION;                                                                  // Many possible logic gates
 
 
 typedef struct Node{
-    
-      int input_edge[MAX_FAN_IN];
-      int output_edge;
+
+      char input_edge[MAX_FAN_IN][MAX_WORD_LENGTH];
+      char output_edge[MAX_WORD_LENGTH];
       FUNCTION function;
-} Node; 
+
+} Node;
 
 
-
-Node* read_file(char A[]){    // reads a .bench file!
+Node* read_file(char A[], int *num_nodes){    // reads a .bench file!
 
 FILE * f_ptr;
-char c;
 int i=0;
+*num_nodes = 0;
 
-Node* node = malloc(MAX * sizeof(Node));  // Dynamically allocate memory for the nodes
+f_ptr = fopen(A, "r");  // Open the file in read mode
+char line[MAX_LINE];   // MAX Line size of the file
+
+Node* node = malloc(MAX_NODES * sizeof(Node));  // Dynamically allocate memory for the nodes
 
 if (node == NULL) {                       // Exit in case malloc fails for some reason
     printf("Memory allocation failed\n");
     exit(1);
 }
 
-
-f_ptr = fopen(A, "r");
-char line[MAX];   // MAX Line size of the file 
-char *end;  // for the end pointer of strtol() function
-
 if (f_ptr == NULL){
-	
-	printf("Error with opening the netlist file...\n");
+
+    printf("Error with opening the netlist file...\n");
     printf("Process terminated :(\n");
+    free(node);
     exit(1);
-    }
+}
 
 
 while((fgets(line, sizeof(line), f_ptr)) != NULL){
 
 
-    if(strncmp("\n", line, 1) == 0){       // Condition to ignore an empty line in case it exists and move on to the next one 
+
+    int only_whitespace = 1;
+
+    for (int k = 0; line[k] != '\0'; k++) {
+
+        if (line[k] != ' ' &&
+            line[k] != '\t' &&
+            line[k] != '\n' &&
+            line[k] != '\r') {
+
+            only_whitespace = 0;
+            break;
+        }
+    }
+
+    if (only_whitespace) {
         continue;
-    
-    }                    
+    }
+
+
+    if(strncmp("\n", line, 1) == 0){       // Condition to ignore an empty line in case it exists and move on to the next one
+
+        continue;
+    }
 
     if(strncmp("#", line, 1) == 0){       // It is for ignoring the first line with the name of the .bench file
 
@@ -64,59 +83,97 @@ while((fgets(line, sizeof(line), f_ptr)) != NULL){
     }
 
 
-    if(strncmp("INPUT", line, 5) == 0){    // I check wether this line describes an input node 
-          node[i].input_edge[0] = -1;
-          node[i].output_edge = strtol((strtok(line, " (),=")), &end, 10);
+    if(strncmp("INPUT", line, 5) == 0){    // I check wether this line describes an input node
+
+          for(int j = 0; j < MAX_FAN_IN; j++){
+              node[i].input_edge[j][0] = '\0';
+          }
+
+          strtok(line, " (),=\n\t");
+          strcpy(node[i].output_edge, strtok(NULL, " (),=\n\t"));
+
           node[i].function = INPUT;
     }
 
-    
+
     else if(strncmp("OUTPUT", line, 6) == 0){  // I check wether this line describes an output node
-        
-        node[i].input_edge[0] = strtol(strtok(line, " (),="), &end, 10);
-        node[i].output_edge = -1;
+
+        for(int j = 0; j < MAX_FAN_IN; j++){
+            node[i].input_edge[j][0] = '\0';
+        }
+
+        strtok(line, " (),=\n\t");
+        strcpy(node[i].input_edge[0], strtok(NULL, " (),=\n\t"));
+
+        node[i].output_edge[0] = '\0';
         node[i].function = OUTPUT;
 
     }
 
+
     else{
-      node[i].output_edge = strtol((strtok(line, " (),=")), &end, 10);
-      char *buffer = strtok(NULL, " (),=");
-      node[i].function = 
-      strcmp(buffer, "AND") == 0 ? AND   : 
-      strcmp(buffer, "OR") == 0 ? OR     : 
-      strcmp(buffer, "NOT") == 0 ? NOT   : 
-      strcmp(buffer, "NAND") == 0 ? NAND : 
-      strcmp(buffer, "NOR") == 0 ? NOR   : 
-      strcmp(buffer, "XOR") == 0 ? XOR   : 
+
+      strcpy(node[i].output_edge, strtok(line, " (),=\n\t"));  // Now i check all the lines that describe a logic gate
+      char *buffer = strtok(NULL, " (),=\n\t");                // and store the output edge of the node in the struct
+
+      node[i].function =
+      strcmp(buffer, "AND") == 0 ? AND   :
+      strcmp(buffer, "OR") == 0 ? OR     :
+      strcmp(buffer, "NOT") == 0 ? NOT   :
+      strcmp(buffer, "NAND") == 0 ? NAND :
+      strcmp(buffer, "NOR") == 0 ? NOR   :
+      strcmp(buffer, "XOR") == 0 ? XOR   :
       strcmp(buffer, "XNOR") == 0 ? XNOR : BUF;
-      
-      char *buff; 
+
+      char *buff;
       int j = 0;
 
-      while(j < MAX_FAN_IN && (buff = strtok(NULL, " (),=")) != NULL){ 
+      while(j < MAX_FAN_IN && (buff = strtok(NULL, " (),=\n\t")) != NULL){
 
-          node[i].input_edge[j] = strtol(buff, &end, 10);
+          strcpy(node[i].input_edge[j], buff);
           j++;
       }
-    }
-    
-    i = i+1;
 
-    if (i >= MAX) {
-        printf("ERROR: Too many nodes!\n");
+
+      while(j < MAX_FAN_IN){
+
+        node[i].input_edge[j][0] = '\0';
+        j++;
+
+      }
+
+    }
+
+    i = i+1;
+    (*num_nodes)++;
+
+    if(i % MAX_NODES == 0){
+
+     Node *temp = realloc(node, (i + MAX_NODES) * sizeof(Node));  // Dynamically reallocate memory for the nodes if we reach the limit
+
+
+    if (temp == NULL) {
+
+        free(node);
+        fclose(f_ptr);
+
+        printf("Sometning went wrong with reallocating memory for the nodes\n");
+
         exit(1);
     }
+
+
+    node = temp;
+
+    }
+
 }
 
 fclose(f_ptr);    // We got everything we needed from the file, so we close it.
 
-  return node;  // Return the dynamically allocated memory for the nodes
-
+return node;  // Return the dynamically allocated memory for the nodes
 
 }   // end of read_file()
-
-
 
 
 
@@ -124,28 +181,41 @@ fclose(f_ptr);    // We got everything we needed from the file, so we close it.
 
 
 
-int main(int argc, char * argv[]){          //Only use main() for debugging. Will get rid of it later.
- 
+int main(int argc, char *argv[]) {          // Only use main() for debugging. Will get rid of it later.
 
-Node *node = read_file(argv[1]);  // Call read_file() and store the returned pointer
 
-for (int i = 0; i < 400; i++) {
+    if(argc < 2){
 
-    printf("Node %d: output_edge = %d, function = %d\n",
-           i,
-           node[i].output_edge,
-           node[i].function);
+        printf("Usage: %s <file.bench>\n", argv[0]);
 
-    for (int j = 0; j < MAX_FAN_IN; j++) {
-        printf("    input_edge[%d] = %d\n",
-               j,
-               node[i].input_edge[j]);
+        return 1;
     }
-}
 
 
-free(node);  // Free the dynamically allocated memory for the nodes
+    int num_nodes;
 
-return 0;
+    Node *node = read_file(argv[1], &num_nodes);
 
+
+    for (int i = 0; i < num_nodes; i++) {
+
+        printf("Node %d: output_edge = %s, function = %d\n",
+               i,
+               node[i].output_edge,
+               node[i].function);
+
+
+        for (int j = 0; j < MAX_FAN_IN; j++) {
+
+            printf("    input_edge[%d] = %s\n",
+                   j,
+                   node[i].input_edge[j]);
+        }
+
+    }
+
+
+    free(node);
+
+    return 0;
 }
